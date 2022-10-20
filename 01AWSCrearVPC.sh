a13a19a1707a64308fe3e9e742fd4de024713338
Javier Terán González <javier.teran@educantabria.es>
2022-10-20 17:53:41 +0200

    EC2 Ubuntu

--- 01AWSCrearVPC.sh
+++ 01AWSCrearVPC.sh
@@ -13,32 +13,38 @@
 AWS_VPC_CIDR_BLOCK=10.22.0.0/16
 AWS_Subred_CIDR_BLOCK=10.22.130.0/24
 
-## Crear una VPC (Virtual Private Cloud)
+## Crear una VPC (Virtual Private Cloud) con su etiqueta
+## La VPC tendrá un bloque IPv4 proporcionado por el usuario y uno IPv6 de AWS
 AWS_ID_VPC=$(aws ec2 create-vpc \
---cidr-block $AWS_VPC_CIDR_BLOCK \
---query 'Vpc.{VpcId:VpcId}' \
---output text)
+  --cidr-block $AWS_VPC_CIDR_BLOCK \
+  --amazon-provided-ipv6-cidr-block \
+  --tag-specification ResourceType=vpc,Tags=[{Key=Name,Value=SRIXX-vpc}]
+  --query 'Vpc.{VpcId:VpcId}' \
+  --output text)
 
 ## Habilitar los nombres DNS para la VPC
 aws ec2 modify-vpc-attribute \
---vpc-id $AWS_ID_VPC \
---enable-dns-hostnames "{\"Value\":true}"
+  --vpc-id $AWS_ID_VPC \
+  --enable-dns-hostnames "{\"Value\":true}"
 
-## Crear una subred publica
+## Crear una subred publica con su etiqueta
 AWS_ID_SubredPublica=$(aws ec2 create-subnet \
---vpc-id $AWS_ID_VPC --cidr-block $AWS_Subred_CIDR_BLOCK \
---availability-zone us-east-1a --query 'Subnet.{SubnetId:SubnetId}' \
---output text)
+  --vpc-id $AWS_ID_VPC --cidr-block $AWS_Subred_CIDR_BLOCK \
+  --availability-zone us-east-1a \
+  --tag-specifications ResourceType=subnet,Tags=[{Key=Name,Value=SRIXX-subred-publica}] \
+  --query 'Subnet.{SubnetId:SubnetId}' \
+  --output text)
 
 ## Habilitar la asignación automática de IPs públicas en la subred pública
 aws ec2 modify-subnet-attribute \
---subnet-id $AWS_ID_SubredPublica \
---map-public-ip-on-launch
+  --subnet-id $AWS_ID_SubredPublica \
+  --map-public-ip-on-launch
 
-## Crear un Internet Gateway (Puerta de enlace)
+## Crear un Internet Gateway (Puerta de enlace) con su etiqueta
 AWS_ID_InternetGateway=$(aws ec2 create-internet-gateway \
---query 'InternetGateway.{InternetGatewayId:InternetGatewayId}' \
---output text)
+  --tag-specifications ResourceType=internet-gateway,Tags=[{Key=Name,Value=SRIXX-igw}]
+  --query 'InternetGateway.{InternetGatewayId:InternetGatewayId}' \
+  --output text)
 
 ## Asignar el Internet gateway a la VPC
 aws ec2 attach-internet-gateway \
@@ -53,37 +59,21 @@ AWS_ID_TablaRutas=$(aws ec2 create-route-table \
 
 ## Crear la ruta por defecto a la puerta de enlace (Internet Gateway)
 aws ec2 create-route \
---route-table-id $AWS_ID_TablaRutas \
---destination-cidr-block 0.0.0.0/0 \
---gateway-id $AWS_ID_InternetGateway
+  --route-table-id $AWS_ID_TablaRutas \
+  --destination-cidr-block 0.0.0.0/0 \
+  --gateway-id $AWS_ID_InternetGateway
 
 ## Asociar la subred pública con la tabla de rutas
 AWS_ROUTE_TABLE_ASSOID=$(aws ec2 associate-route-table  \
---subnet-id $AWS_ID_SubredPublica \
---route-table-id $AWS_ID_TablaRutas \
---output text)
-
-## Asignación de etiquetas a los recursos
-## Añadir etiqueta a la VPC
-aws ec2 create-tags \
---resources $AWS_ID_VPC \
---tags "Key=Name,Value=SRIXX-vpc"	
- 
-## Añaadir etiqueta a la subred pública
-aws ec2 create-tags \
---resources $AWS_ID_SubredPublica \
---tags "Key=Name,Value=SRIXX-subred-publica"
-
-## Añadir etiqueta a la puerta de enlace
-aws ec2 create-tags \
---resources $AWS_ID_InternetGateway \
---tags "Key=Name,Value=SRIXX-igw"
+  --subnet-id $AWS_ID_SubredPublica \
+  --route-table-id $AWS_ID_TablaRutas \
+  --output text)
 
 ## Añadir etiqueta a la ruta por defecto
 AWS_DEFAULT_ROUTE_TABLE_ID=$(aws ec2 describe-route-tables \
---filters "Name=vpc-id,Values=$AWS_ID_VPC" \
---query 'RouteTables[?Associations[0].Main != `flase`].RouteTableId' \
---output text) &&
+  --filters "Name=vpc-id,Values=$AWS_ID_VPC" \
+  --query 'RouteTables[?Associations[0].Main != `flase`].RouteTableId' \
+  --output text) &&
 aws ec2 create-tags \
 --resources $AWS_DEFAULT_ROUTE_TABLE_ID \
 --tags "Key=Name,Value=SRIXX ruta por defecto"
